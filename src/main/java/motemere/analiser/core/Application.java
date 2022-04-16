f3636fe6fd896650be7ba328c0d81109fbe79ab7
Dmitry Turenko (PC) <dturenko@gmail.com>
2022-04-16 11:57:59 +0300

    add google-style checkstyle with some edits thats caused failure. Correct classes with checkstyle except javadoc

--- src/main/java/motemere/analiser/core/Application.java
+++ src/main/java/motemere/analiser/core/Application.java
@@ -1,7 +1,5 @@
 package motemere.analiser.core;
 
-import motemere.analiser.core.MessageClass;
-
 class Application {
   
   public static void main(String[] args) {
--- src/main/java/motemere/analiser/core/MessageClass.java
+++ src/main/java/motemere/analiser/core/MessageClass.java
@@ -2,10 +2,9 @@ package motemere.analiser.core;
 
 public class MessageClass { 
 
-    private static final String TEXT = "Simple message text"; 
+  private static final String TEXT = "Simple message text"; 
 
-    public static String get() {
-        return TEXT;
-    }
-    
+  public static String get() {
+    return TEXT;
+  }
 }
